555f496e493bd99e739a2e74ece38f9729e3f130
irizisa <irizisa@icloud.com>
2023-08-16 20:02:36 +0300

    first swift codes i learned

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+// Variables & Constants
+
+//snake_case
+//camelCase
+
+// PART 1
+// String
+var userName = "James"
+userName.append("o")
+userName.lowercased()
+userName.uppercased()
+
+//Integer & Double & Float
+
+//Int
+let userAge = 50
+let myNumber = 4
+userAge / 4
+
+//Double
+let UserAgeD = 50.0
+let myNumberD = 4.0
+UserAgeD / myNumberD
+
+//Boolean
+
+var myBoolean = false
+myBoolean = true
+
+// PART 2
+
+let myString : String = "50"
+myString.uppercased()
+let anotherNumber : Double = Double(50.5)
+let anotherNumber2 : Int = Int(50.5)
+anotherNumber * 1.5
+
+// define tanımlama
+let myvariable : String
+// initialization başlatma
+myvariable="selam"
+
+let myUpperVariable = myvariable.uppercased()
+print(myvariable)
+print(myUpperVariable)
+

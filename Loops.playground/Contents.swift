d6589c1ce2c3ceab9056c136b850e58e3353356e
irizisa <irizisa@icloud.com>
2023-08-29 10:38:30 +0300

    Update Contents.swift

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -14,13 +14,42 @@ var numara = 0
 // While Loop
 while numara <= 15
 {
-    print(numara)
+    //print(numara)
     numara+=1
 }
 
 var characterAlive = true
 while characterAlive == true
 {
-    print("karakter yaşıyor")
+  //  print("karakter yaşıyor")
     characterAlive = false
+}
+
+var sayi = 0
+while sayi <= 10 {
+  //  print(sayi)
+    sayi = sayi + 1
+    
+}
+
+/*
+ FOR LOOP ** DAHA COK DİZİLERLE KULLANIYORUZ **
+ */
+var fruitArray = ["muz","portakal","elma"]
+
+for fruite in fruitArray
+{
+    print(fruite)
+}
+
+var myNumbers = [10,20,30,40,50,60]
+
+for num in myNumbers
+{
+    print(num / 5)
+}
+
+for num2 in 1 ... 5
+{
+    print(num2*5)
 }
